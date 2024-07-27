caa91f35b48de077924d86a8875c3e4a64eccc2d
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-27 16:18:16 +0200

    with env

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -8,6 +8,19 @@ interface IAppProvider {
 	children: React.ReactNode;
 }
 
+const githubAccessToken = import.meta.env.VITE_GITHUB_ACCESS_TOKEN;
+
+const githubInfos = {
+	baseURL: "https://api.github.com/graphql",
+	username: "edwardtanguay",
+	headers: {
+		"Content-Type": "applciation/json",
+		Authorization: `bearer ${githubAccessToken}`
+	}
+}
+
+console.log(githubInfos);
+
 export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
